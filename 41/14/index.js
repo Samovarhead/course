aa34397a5e8b7ddb1a50a61543cc9d79c6e357dd
Denis <bugname26@gmail.com>
2019-05-23 21:13:58 +0300

    подапки урок 14

--- 41/14/index.js
+++ 41/14/index.js
@@ -1,11 +1,31 @@
+// var http = require('http');
+// var port = 3000;
+
+// var app = http.createServer(function(req, res) {
+// 	console.log(req.url);
+// 	console.log(req.method);
+// 	res.write('hello');
+// 	res.end();
+// });
+
+// app.listen(port, function() {
+// 	console.log('Server at http://localhost:3000');
+// });
 var http = require('http');
+var port = 3000;
 
 var app = http.createServer(function(req, res) {
-	res.write('hello');
-	res.end();
+	if (req.url === '/') {
+		res.write('hello');
+ 		res.end();
+	}else if (req.url === '/contact') {
+		res.write('hello contact');
+ 		res.end();
+	}
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
 	console.log('Server at http://localhost:3000');
 });
+
 
